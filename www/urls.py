528e9530fc22089da7705b6fe51d30d4b62d5069
paul huang <paul_hzq@hotmail.com>
2015-08-21 17:50:34 -0400

    change urls.py file

--- www/urls.py
+++ www/urls.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-__author__ = 'Paul Huang'
-import logging
-from transwarp.web import get, view
+
+__author__ = 'Michael Liao'
+
+import os, re, time, base64, hashlib, logging
+
+from transwarp.web import get, post, ctx, view, interceptor, seeother, notfound
+
 from models import User, Blog, Comment
-@view('test_users.html')
+
+@view('blogs.html')
 @get('/')
-def test_users():
-	users = User.find_all()
-	return dict(users=users)
+def index():
+    blogs = Blog.find_all()
+    user = User.find_first('where email=?', 'admin@example.com')
+    return dict(blogs=blogs, user=user)
+
